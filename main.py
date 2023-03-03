28f40bdf615f81e5c01081af1273f108ebd6fe6f
R-Shyam-sundar <82640234+R-Shyam-sundar@users.noreply.github.com>
2023-03-03 11:33:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,10 @@
 	Alpha-Beta Pruning on Tic-Tac-Toe Game - Game Tree
 '''
 
+import sys
+
+sys.setrecursionlimit(1500)
+
 class Tic_tac_toe:
 	def __init__(self):
 		self.print_rules()
@@ -13,15 +17,16 @@ class Tic_tac_toe:
 		self.moves = 0
 		self.print_board()	
 		self.start()
+		self.computer_move_cnt = 0
+		self.opponent_move_cnt = 0
 		pass
 
 	def print_rules(self):
-		print("Welcome to Tic-Tac-Toe Game");
+		print("\nWelcome to Tic-Tac-Toe Game");
 		print("The rules of the game are as follows: ")
 		print("1. The coordinates in this game are 0-indexed")
 		print("2. The program plays with character 'O' and you play with character 'X'")
-		print("3. At each turn type two space separated integers denoting the coordinates in which you play 'X'")
-		print("4. The coordinates")
+		print("3. At each turn type two space separated integers denoting the coordinates in which you play 'X'\n")
 		for i in range(0,3):
 			print("|",end="")
 			for j in range(0,3):
@@ -30,19 +35,19 @@ class Tic_tac_toe:
 		pass
 
 	def print_board(self):
-		print("The Board: ")
+		print("\nThe Board: ")
 		for i in range(0,3):
 			print("|",end="")
 			for j in range(0,3):
 				print(f"{self.board[i][j]}",end="|")
 			print("",end="\n")
-		print(f"MOVES COUNT : [{self.moves} moves completed]")
+		print(f"\nMOVES COUNT : [{self.moves} moves completed]")
 		pass
 
 	def start(self):
 		choice = 3;
 		while choice<1 or choice>2:
-			choice = int(input("You start the game: 1\nComputer Starts the game: 2\nYour choice : "))
+			choice = int(input("\nYou start the game: 1\nComputer Starts the game: 2\n\nYour choice : "))
 			if(choice<1 or choice>2):
 				print("ERROR: Enter a valid choice")
 				pass
@@ -54,6 +59,13 @@ class Tic_tac_toe:
 		pass
 
 	def opponent_move(self,move):
+		res = self.check_win()
+		if res == 3:
+			return 1
+		elif res == 1:
+			return 0
+		elif res == 2:
+			return -1
 		mn = 1000
 		x = -1
 		y = -1
@@ -61,24 +73,23 @@ class Tic_tac_toe:
 			for j in range(0,3):
 				if self.board[i][j] == '*':
 					self.board[i][j] = 'X'
-					val = computer_move(move+1)
+					val,tx,ty = self.computer_move(move+1)
 					if val<mn:
 						mn = val
 						x = i
 						y = j
 					self.board[i][j] = '*'
-		return x,y
+		return mn
 		pass
 	
 	def computer_move(self,move):
-		if move == 9:
-			res = self.check_win()
-			if res == 3:
-				return 1
-			elif res == 1:
-				return 0
-			else:
-				return -1
+		res = self.check_win()
+		if res == 3:
+			return 1,-1,-1
+		elif res == 1:
+			return 0,-1,-1
+		elif res == 2:
+			return -1,-1,-1
 		mx = -1000
 		x = -1
 		y = -1
@@ -86,23 +97,45 @@ class Tic_tac_toe:
 			for j in range(0,3):
 				if self.board[i][j] == '*':
 					self.board[i][j] = 'O'
-					val = opponent_move(move+1)
+					val = self.opponent_move(move+1)
 					if val>mx:
 						mx = val
 						x = i
 						y = j
 					self.board[i][j] = '*'
-		return x,y
+		return mx,x,y
 		pass
 
 	def myself(self):
 		# Apply Alpha-Beta Pruning to find the optimal value
-		x,y = self.computer_move(self.moves)
+		res = self.check_win()
+		if res == 1: #game ended in draw
+			print("RESULT: The Game has ended in DRAW")
+			return
+		elif res == 2: #opponent won the game
+			print("RESULT: Congratulations you won the game")
+			return
+		elif res == 3: #computer won the game
+			print("RESULT: Computer has won the game")
+			return
+		val,x,y = self.computer_move(self.moves)
 		self.moves += 1
-
+		self.board[x][y] = 'O'
+		self.print_board()
+		self.opponent()
 		pass
 
 	def opponent(self):
+		res = self.check_win()
+		if res == 1: #game ended in draw
+			print("RESULT: The Game has ended in DRAW")
+			return
+		elif res == 2: #opponent won the game
+			print("RESULT: Congratulations you won the game")
+			return
+		elif res == 3: #computer won the game
+			print("RESULT: Computer has won the game")
+			return
 		self.moves += 1
 		print("Enter the coordinates (space separated):  ",end="")
 		x,y = map(int,input().split())
@@ -115,21 +148,10 @@ class Tic_tac_toe:
 				self.board[x][y] = 'X'
 				break
 		self.print_board()
-		res = self.check_win()
-		if res == 0: #game in progress 
-			self.myself()
-		elif res == 1: #game ended in draw
-			print("RESULT: The Game has ended in DRAW")
-		elif res == 2: #opponent won the game
-			print("RESULT: Congratulations you won the game")
-		elif res == 3: #computer won the game
-			print("RESULT: Computer has won the game")
+		self.myself()
 		pass
 
 	def check_win(self):
-		#Game is still in progress
-		if self.moves < 9:
-			return 0
 		
 		#Check if 'X' won the game
 		for j in range(0,3):
@@ -175,8 +197,10 @@ class Tic_tac_toe:
 		if cnt_d1==3 or cnt_d2==3:
 			return 3
 
-		#Game ended in a draw
-		return 1
+		if self.moves < 9:     #Game in progress
+			return 0
+		else:                  #Game ended in a draw
+			return 1
 
 
 def main():
